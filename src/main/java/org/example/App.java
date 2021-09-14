d64ad9b12688e4427954928b2955fdb52eafde39
rusty <rusty@rustys-MacBook-Air.local>
2021-09-13 22:17:41 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,38 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 14 Solution
+ *  Copyright 2021 Rebecca Pina Partidas
+ */
+
+// Libraries
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        Scanner numInput = new Scanner(System.in);
+        Scanner stringInput = new Scanner(System.in);
+        double tax = 0;
+        double total = 0;
+
+        // Calculates tax and total
+        // If state is WI add a tax
+        System.out.print("What is the order amount? ");
+        double orderAmt = numInput.nextDouble();
+        System.out.print("What is the state? ");
+        String state = stringInput.nextLine();
+
+        if (state.equals("WI"))
+        {
+            tax = 0.055 * orderAmt;
+        }
+
+        total = orderAmt + tax;
+
+        System.out.format("The subtotal is $%.2f. \n", orderAmt);
+        System.out.format("The tax is $%.2f. \n", tax);
+        System.out.format("The total is $%.2f. \n", total);
+    }
+}
